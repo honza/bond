1dfa459705736303e347d61cf199410b23350ad4
Honza Pokorny <me@honza.ca>
2012-06-12 10:13:36 -0300

    Silly

--- server/project.clj
+++ server/project.clj
@@ -1,7 +1,6 @@
 (defproject bond "0.1.0-SNAPSHOT"
             :description "The spy agent"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [org.clojars.tavisrudd/redis-clojure "1.3.1"]
                            [carmine "0.8.2-SNAPSHOT"]
                            [cheshire "4.0.0"]
                            [clj-time "0.4.2"]
